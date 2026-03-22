c8830531a8a743c1e84c5d1beed8220c8b307a92
WeiLiu1616 <750696481@qq.com>
2026-03-22 18:18:31 +0800

    Update vite.config.ts
    解决404网页

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // 添加下面这一行，使用相对路径，这能完美适配 GitHub Pages 的子目录结构
+  base: "./", 
+  
   server: {
     host: "::",
     port: 8080,
@@ -12,7 +15,10 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
